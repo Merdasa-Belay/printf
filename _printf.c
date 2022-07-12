3d8d069d171f4b56f0a2d535f0562db3a9dddbf0
Merdasa-Belay <merdasabelay@gmail.com>
2022-07-12 19:21:49 +0300

    produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -10,18 +10,28 @@
  */
 int _printf(const char *format, ...)
 {
-	
-	va_list arguments;
+const char *p;
+va_list arguments;
 
-	register int count = 0;
+register int count = 0;
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+va_start(arguments, format);
+if (!format || (format[0] == '%' && !format[1]))
+return (-1);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
+for (p = format; *p; p++)
+{
+if (*p == '%')
+{
+p++;
+if (*p == '%')
+{
+count += _putchar('%');
+}
+}
+}
+_putchar(-1);
+va_end(arguments);
+return (count);
 }
